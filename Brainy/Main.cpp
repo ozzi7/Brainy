9bd48f9e351cf2f197206def4960242992f0fc44
ozzi7 <nielregen@gmail.com>
2021-06-11 23:42:37 +0200

    Update Main.cpp

--- Brainy/Main.cpp
+++ Brainy/Main.cpp
@@ -1,4 +1,4 @@
-// Brainy.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// Main.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
 #include <iostream>
